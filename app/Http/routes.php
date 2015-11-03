d725253ac15db7853b1912dc92ca8b2a793b9ce3
Marios Fakiolas <m.fakiolas@hotmail.gr>
2015-11-03 23:28:01 +0200

    fix dependencies / sass / remove extra code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,3 @@
 <?php
 
-Route::group(['namespace' => 'API', 'prefix' => 'api'], function() {
-	resource('recipes', 'RecipesController', ['only' => ['index', 'show']]);
-	resource('topics', 'TopicsController', ['only' => ['index', 'show']]);
-	resource('posts', 'PostsController', ['only' => ['index', 'show']]);
-});
-
-resource('recipes', 'RecipesController', ['only' => ['index', 'show']]);
-resource('topics', 'TopicsController', ['only' => ['index', 'show']]);
-resource('contact', 'ContactController', ['only' => ['store']]);
-resource('newsletter', 'NewsletterController', ['only' => ['store']]);
-get('error', ['as' => 'error', 'uses' => 'PagesController@error']);
 get('/', ['as' => 'home', 'uses' => 'PagesController@home']);
--- resources/views/layouts/pages/header.blade.php
+++ resources/views/layouts/pages/header.blade.php
@@ -18,24 +18,3 @@
 </head>
 
 <body ng-app="recipesApp" ng-cloak>
-	<div class="navbar-fixed">
-		<nav class="grey darken-4" role="navigation" ng-controller="NavigationController as navigationCtl">
-			<div class="nav-wrapper container">
-				<a id="logo-container" href="#!/" class="brand-logo waves-effect waves-light">Angular<span class="text-red">JS</span>
-				Recipes</a>
-				<ul class="right hide-on-med-and-down">
-					<li ng-class="{ active: navigationCtl.isCurrentPath('/recipes') }"><a href="#!/recipes" class="waves-effect waves-light">Recipes</a></li>
-					<li ng-class="{ active: navigationCtl.isCurrentPath('/blog') }"><a href="#!/blog" class="waves-effect waves-light">News</a></li>
-					<li><a href="#contactModal" class="waves-effect waves-light" modal>Contact</a></li>
-				</ul>
-
-				<ul id="nav-mobile" class="side-nav">
-					<li ng-class="{ active: navigationCtl.isCurrentPath('/') }"><a href="#!/" class="waves-effect waves-light">Home</a></li>
-					<li ng-class="{ active: navigationCtl.isCurrentPath('/recipes') }"><a href="#!/recipes" class="waves-effect waves-light">Recipes</a></li>
-					<li ng-class="{ active: navigationCtl.isCurrentPath('/blog') }"><a href="#!/blog" class="waves-effect waves-light">News</a></li>
-					<li><a href="#contactModal" class="waves-effect waves-light" modal>Contact</a></li>
-				</ul>
-				<a href="#" class="button-collapse" data-activates="nav-mobile" data-sidenav="left" data-closeonclick="true"><i class="mdi-navigation-menu"></i></a>
-			</div>
-		</nav>
-	</div>
