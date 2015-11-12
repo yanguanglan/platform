13344f165defe7a68a114dbe1ecf3d94aae0a54f
samexe <voumellis@gmail.com>
2015-11-12 21:44:20 +0200

    beta & logo

--- resources/views/layouts/pages/footer.blade.php
+++ resources/views/layouts/pages/footer.blade.php
@@ -2,8 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col l6 s12">
-				<h5 class="white-text">Angular<span class="text-red">JS</span> Recipes</h5>
-
+				<h5 class="white-text fbeta">Angular<span class="text-red">JS</span> Recipes</h5>
 				<p class="grey-text text-lighten-4">This is a non-profitable project created by developers who are tired
 					of paying for knowledge, experiences, recipes and tricks. Share your recipes and help build a strong
 					community. Because knowledge is power!</p>
@@ -31,9 +30,12 @@
 	</div>
 	<div class="footer-copyright">
 		<div class="container">
-			Angular<span class="text-red">JS</span> Recipes &copy; 2015
+			<span class="ibeta">
+			Angular<span class="text-red">JS</span> Recipes &copy; 2015 
 			{{--<a class="grey-text text-lighten-4 right" href="#!">More Links</a>--}}
-		</div>
+			</span>
+		</div> 
+		
 	</div>
 </footer>
 
