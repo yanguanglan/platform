7b9402731adb81c56fe77bc54dfcb666ab89306d
Marios Fakiolas <m.fakiolas@hotmail.gr>
2015-11-12 13:51:43 +0200

    add exercises codepen examples / add tooltips info

--- app/Exercise.php
+++ app/Exercise.php
@@ -8,9 +8,16 @@ class Exercise extends Model {
 
 	public $timestamps = false;
 
+    public $appends = ['codepen_url'];
+
 	public function recipe()
 	{
 		return $this->belongsTo('App\Recipe');
+	}
+
+    public function getCodepenUrlAttribute()
+	{
+		return $this->attributes['codepen'] ? 'http://codepen.io/angularjs-recipes/pen/' . $this->attributes['codepen'] : null;
 	}
 
 }
