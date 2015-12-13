9ec13c057c5a49c8cab88ccc94894c153b6136f4
Marios Fakiolas <m.fakiolas@hotmail.gr>
2015-12-13 02:33:17 +0200

    fix gravatar update

--- public/js/controllers/public/navigation.js
+++ public/js/controllers/public/navigation.js
@@ -5,9 +5,9 @@
 		.module('recipesApp')
 		.controller('NavigationController', NavigationController);
 
-	NavigationController.$inject = ['authService', '$auth', '$scope', '$rootScope', '$location', 'md5'];
+	NavigationController.$inject = ['authService', '$auth', '$rootScope', '$location', 'md5'];
 
-	function NavigationController(authService, $auth, $scope, $rootScope, $location, md5) {
+	function NavigationController(authService, $auth, $rootScope, $location, md5) {
 		var navCtl = this;
 		navCtl.user = authService.isLoggedIn();
 		navCtl.logout = function() {
@@ -18,18 +18,14 @@
 					navCtl.user = null;
 				});
 		};
+		navCtl.gravatar = navCtl.user ? 'http://www.gravatar.com/avatar/' + md5.createHash(navCtl.user.email) + '&s=80' : null;
 		navCtl.isAuthenticated = function() {
 			return $auth.isAuthenticated();
 		};
 
 		$rootScope.$on('update', function(event, data) {
 			navCtl.user = data;
-		});
-
-		$scope.$watch(angular.bind(navCtl, function() {
-			return navCtl.user.email;
-		}), function(newVal, oldVal) {
-			navCtl.gravatar = navCtl.user ? 'http://www.gravatar.com/avatar/' + md5.createHash(navCtl.user.email) + '&s=80' : null;
+			navCtl.gravatar = 'http://www.gravatar.com/avatar/' + md5.createHash(navCtl.user.email) + '&s=80';
 		});
 	}
 })();
